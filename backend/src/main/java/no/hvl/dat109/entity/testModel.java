f5dee88cba26d2b7cd61127beeec99b64b117c18
MrHencke <hencke1@hotmail.com>
2021-03-09 13:58:24 +0100

    Utils added

--- backend/src/main/java/no/hvl/dat109/entity/testModel.java
+++
@@ -1,9 +0,0 @@
-package no.hvl.dat109.entity;
-
-public class testModel {
-
-    /*
-     * Test DB functionality later with this class.
-     */
-
-}
--- backend/src/main/java/no/hvl/dat109/util/JWTUtil.java
+++ backend/src/main/java/no/hvl/dat109/util/JWTUtil.java
@@ -5,50 +5,54 @@ import java.util.Map;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
-import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTDecodeException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 
 public class JWTUtil {
 
     public String createToken(String id) {
-    	
-    	String token = null;
-    	Map<String, Object> payloadClaims = new HashMap<>();
-    	payloadClaims.put("id", id);
-    	
+
+        String token = null;
+        Map<String, Object> payload = new HashMap<>();
+        payload.put("id", id);
+
         try {
-            Algorithm algorithm = Algorithm.HMAC256("secret"); //Change secret and move to variables file
-            token = JWT.create()
-                .withIssuer("Gruppe-10")
-                .withPayload(payloadClaims)
-                .sign(algorithm);
-        } catch (JWTCreationException exception){
-            //Add error handling
-        	exception.printStackTrace();
-        }
-        
-        return token;
-    
-    }
-    
-    public boolean verifyToken(String token){
-        
-    	try {
-            Algorithm algorithm = Algorithm.HMAC256("secret"); //Change secret and move to variables file
-            JWTVerifier verifier = JWT.require(algorithm)
-                .withIssuer("Gruppe-10")
-                .build(); //Reusable verifier instance
-            DecodedJWT jwt = verifier.verify(token);
-            
-        } catch (JWTVerificationException exception){
-            // Handle invalid signature
-        	return false;
+            token = JWT.create().withIssuer(Config.Issuer).withPayload(payload).sign(Config.algorithm);
+        } catch (JWTCreationException exception) {
+            // Add error handling
+            exception.printStackTrace();
         }
 
-        //Continue if valid
+        return token;
+
+    }
+
+    public boolean verifyToken(String token) {
+
+        try {
+            JWTVerifier verifier = JWT.require(Config.algorithm).withIssuer(Config.Issuer).build(); // Reusable verifier
+            DecodedJWT decodedJWT = verifier.verify(token);
+
+        } catch (JWTVerificationException exception) {
+            // Handle invalid signature
+            return false;
+        }
+
+        // Continue if valid
         return true;
-    
+
+    }
+
+    public DecodedJWT decodeJWT(String token) {
+        DecodedJWT decoded = null;
+        try {
+            decoded = JWT.decode(token);
+        } catch (JWTDecodeException exception) {
+            // Handle invalid token
+            exception.printStackTrace();
+        }
+        return decoded;
     }
 }
