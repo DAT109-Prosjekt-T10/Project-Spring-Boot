2031faf41d877b6012e1ff7fec4be2cbb31b821b
Markus Bjermeland <markus.bjermeland@zdata.no>
2021-03-09 11:36:34 +0100

    updated reducer

--- frontend/src/store/reducers/authors/authorReducer.js
+++ frontend/src/store/reducers/authors/authorReducer.js
@@ -13,51 +13,6 @@ const initialState = {
 
 const authorReducer = (state = initialState, action) => {
 	switch (action.type) {
-		case GET_RECEIVER_STARTED:
-			return {
-				...state,
-				loading: true,
-			}
-		case GET_RECEIVER_SUCCESS:
-			return {
-				...state,
-				data: action.payload.data,
-				loading: false,
-			}
-		case GET_RECEIVER_FAILURE:
-			return {
-				...state,
-				loading: false,
-				error: action.payload.error,
-			}
-		case UPDATE_RECEIVER_PROFILE_STARTED:
-			return {
-				...state,
-				update: {
-					loading: true,
-					success: false,
-					error: null,
-				},
-			}
-		case UPDATE_RECEIVER_PROFILE_SUCCESS:
-			return {
-				...state,
-				data: action.payload.data,
-				update: {
-					loading: false,
-					success: true,
-					error: null,
-				},
-			}
-		case UPDATE_RECEIVER_PROFILE_FAILURE:
-			return {
-				...state,
-				update: {
-					loading: false,
-					success: false,
-					error: action.payload.error,
-				},
-			}
 		default:
 			return state
 	}
