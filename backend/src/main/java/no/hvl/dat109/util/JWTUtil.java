0902d01e226e8320575e5d8250b0b95c6565f1f4
andersbuanes <anderstbuanes@hotmail.com>
2021-03-09 13:03:42 +0100

    JWTUtil

--- backend/src/main/java/no/hvl/dat109/util/JWTUtil.java
+++ backend/src/main/java/no/hvl/dat109/util/JWTUtil.java
@@ -1,37 +1,48 @@
 package no.hvl.dat109.util;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+
 public class JWTUtil {
 
-
-    public string createToken() {
+    public String createToken() {
+    	
+    	String token = null;
+    	
         try {
             Algorithm algorithm = Algorithm.HMAC256("secret"); //Change secret and move to variables file
-            String token = JWT.create()
+            token = JWT.create()
                 .withIssuer("Gruppe-10")
                 .sign(algorithm);
         } catch (JWTCreationException exception){
             //Add error handling
+        	exception.printStackTrace();
         }
-
-        return token;
         
+        return token;
+    
     }
 
-
-    public verifyToken(String token){
-        try {
+    public boolean verifyToken(String token){
+        
+    	try {
             Algorithm algorithm = Algorithm.HMAC256("secret"); //Change secret and move to variables file
             JWTVerifier verifier = JWT.require(algorithm)
                 .withIssuer("Gruppe-10")
                 .build(); //Reusable verifier instance
             DecodedJWT jwt = verifier.verify(token);
-
             
         } catch (JWTVerificationException exception){
             // Handle invalid signature
+        	return false;
         }
 
         //Continue if valid
-
+        return true;
+    
     }
 }
