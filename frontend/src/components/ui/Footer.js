b758ff65ea742655e8e9323f98d6633eb27b484c
Markus Bjermeland <markus.bjermeland@zdata.no>
2021-03-16 19:12:34 +0100

    changes to register/login form

--- frontend/src/components/ui/Footer.js
+++ frontend/src/components/ui/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
 	return (
 		<footer className='cs-footer py-4 mt-auto'>
-			<div className='text-md-center'>
+			<div className='text-center'>
 				<p className='font-size-sm mb-0 mr-3 order-md-1'>
 					<span className='text-muted mr-1'>
 						© {new Date().getFullYear()} - Library System
