5c2914f925a5e307aa640d26f8c71a7a6ad33e18
Markus Bjermeland <markus.bjermeland@zdata.no>
2021-03-17 16:50:44 +0100

    removed unused dependencies and added proider store redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './store'
 import Content from './components/content/Content'
 import Header from './components/ui/Header'
 import Footer from './components/ui/Footer'
@@ -10,21 +12,23 @@ import NotFound from './components/ui/NotFound'
 
 const App = () => {
 	return (
-		<Router>
-			<main className='page-wrapper d-flex flex-column min-vh-100'>
-				<Header />
-				<Background />
-				<Switch>
-					<Route exact path='/' component={Content} />
-					<Route exact path='/books' component={Content} />
-					<Route exact path='/authors' component={Content} />
-					<Route exact path='/login' component={Login} />
-					<Route exact path='/register' component={Register} />
-					<Route path='*' component={NotFound} />
-				</Switch>
-				<Footer />
-			</main>
-		</Router>
+		<Provider store={store}>
+			<Router>
+				<main className='page-wrapper d-flex flex-column min-vh-100'>
+					<Header />
+					<Background />
+					<Switch>
+						<Route exact path='/' component={Content} />
+						<Route exact path='/books' component={Content} />
+						<Route exact path='/authors' component={Content} />
+						<Route exact path='/login' component={Login} />
+						<Route exact path='/register' component={Register} />
+						<Route path='*' component={NotFound} />
+					</Switch>
+					<Footer />
+				</main>
+			</Router>
+		</Provider>
 	)
 }
 
