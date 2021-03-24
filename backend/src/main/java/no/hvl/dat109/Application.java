47b79136a7474503edaa4622055346a3496a87d4
MrHencke <hencke1@hotmail.com>
2021-03-24 15:36:34 +0100

    Fix CORS?

--- backend/src/main/java/no/hvl/dat109/Application.java
+++ backend/src/main/java/no/hvl/dat109/Application.java
@@ -4,13 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import no.hvl.dat109.util.Config;
-
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class Application {
 
@@ -21,16 +14,6 @@ public class Application {
 	 */
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins(Config.AllowedOrigins).allowedMethods(Config.AllowedMethods);
-			}
-		};
 	}
 
 }
