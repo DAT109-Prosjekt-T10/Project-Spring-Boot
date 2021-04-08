c310dc89ec728bed85dd1b36e45bc350f903949c
Rune <rune.maestad@gmail.com>
2021-04-08 13:45:53 +0200

    Added error messages to UserController.java

--- backend/src/main/java/no/hvl/dat109/controller/UserController.java
+++ backend/src/main/java/no/hvl/dat109/controller/UserController.java
@@ -2,6 +2,7 @@ package no.hvl.dat109.controller;
 
 import no.hvl.dat109.entity.User;
 import no.hvl.dat109.repository.UserRepository;
+import no.hvl.dat109.util.ApiError;
 import no.hvl.dat109.util.JWTUtil;
 import no.hvl.dat109.util.PasswordUtil;
 import org.apache.commons.validator.routines.EmailValidator;
@@ -27,28 +28,25 @@ public class UserController {
     public ResponseEntity<Object> registerUser(@RequestBody User user) {
 
         if (user.getEmail() == null || user.getPassword() == null || user.getName() == null) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ApiError("One or more user-attribute is null"));
         }
 
         if (!(EmailValidator.getInstance().isValid(user.getEmail()) && (user.getPassword().length() >= 8)
                 && user.getName().length() > 0)) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ApiError("Email/password/name format is invalid."));
         }
 
         // Check if email already exists
         User userWithEmail = userRepository.findByEmail(user.getEmail());
         if (userWithEmail != null) {
             System.out.println(userWithEmail.toString());
-            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(new ApiError("User with email already exists."));
         }
 
         String passwordHash = PasswordUtil.hashPassword(user.getPassword());
 
         User newUser = new User(user.getName(), user.getEmail(), passwordHash, user.isAdmin());
         User savedUser = userRepository.save(newUser);
-
-        // Skjuler passord i JSON-response
-        savedUser.setPassword(null);
 
         String token = JWTUtil.createToken(savedUser.getName(), savedUser.getId(), savedUser.isAdmin());
 
@@ -65,24 +63,24 @@ public class UserController {
     public ResponseEntity<Object> authorizeUser(@RequestBody User user) {
 
         if (user.getEmail() == null || user.getPassword() == null) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ApiError("Email and/or password are null."));
         }
 
         if (!(EmailValidator.getInstance().isValid(user.getEmail()) && (user.getPassword().length() >= 8))) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Email format is invalid and/or password is too short.");
         }
 
         User registeredUser = userRepository.findByEmail(user.getEmail());
 
         // Check if user exists
         if (registeredUser == null) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("User with email does not exist.");
         }
 
         boolean correctPassword = PasswordUtil.comparePassword(user.getPassword(), registeredUser.getPassword());
 
         if (!correctPassword) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ApiError("Invalid login credentials"));
         }
 
         String token = JWTUtil.createToken(registeredUser.getName(), registeredUser.getId(), registeredUser.isAdmin());
